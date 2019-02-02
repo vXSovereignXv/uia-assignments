efaf18894c5dd8d9702edda7a9a9d4b707146b4e
Nick Daigle <nickdaigle66@gmail.com>
2019-02-02 16:41:23 -0600

    Chapter 5 completed

---
+++ ch05/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+
+    public int Id { get; private set; }
+
+    public void SetCard(int id, Sprite image)
+    {
+        Id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        if(cardBack.activeSelf && controller.CanReveal)
+        {
+            cardBack.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+}
