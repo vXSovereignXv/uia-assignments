462965c26ba8b41a7ed07b809fc4b87d2dbc600e
Nick Daigle <nickdaigle66@gmail.com>
2019-02-03 18:41:00 -0600

    Completed chapter 6

---
+++ ch06/Assets/Scripts/PlatformPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayer : MonoBehaviour
+{
+    public float speed = 250.0f;
+    public float jumpForce = 12.0f;
+
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
+
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = hit != null;
+        _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        MovingPlatform platform = null;
+        Vector3 pScale = Vector3.one;
+        if (grounded)
+        {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+        if(platform != null)
+        {
+            transform.parent = platform.transform;
+            pScale = platform.transform.localScale;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            var scaleX = Mathf.Sign(deltaX) / pScale.x;
+            var scaleY = 1 / pScale.y;
+            transform.localScale = new Vector3(scaleX, scaleY, 1);
+        }
+    }
+}
