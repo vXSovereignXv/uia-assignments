fae42018d7211840da5f0a34a673ebbc953a7c3e
Nick Daigle <nickdaigle66@gmail.com>
2019-02-24 22:25:51 -0600

    Start chapter 9

---
+++ ch09/Assets/Scripts/DeviceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] targets;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Activate");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Deactivate");
+        }
+    }
+}
---
+++ ch09/Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;
+    [SerializeField] private float openTime = 1f;
+
+    private bool _open;
+    private bool operating;
+
+    public void Operate()
+    {
+        if (!operating)
+        {
+            if (_open)
+            {
+                Vector3 pos = transform.position - dPos;
+                iTween.MoveTo(gameObject, pos, openTime);
+                StartCoroutine(DisableDoor());
+            }
+            else
+            {
+                Vector3 pos = transform.position + dPos;
+                iTween.MoveTo(gameObject, pos, openTime);
+                StartCoroutine(DisableDoor());
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        if (!operating && !_open)
+        {
+            Vector3 pos = transform.position + dPos;
+            iTween.MoveTo(gameObject, pos, openTime);
+            StartCoroutine(DisableDoor());
+        }
+    }
+
+    public void Deactivate()
+    {
+        if (!operating && _open)
+        {
+            Vector3 pos = transform.position - dPos;
+            iTween.MoveTo(gameObject, pos, openTime);
+            StartCoroutine(DisableDoor());
+        }
+    }
+
+    private IEnumerator DisableDoor()
+    {
+        operating = true;
+        yield return new WaitForSeconds(openTime);
+        operating = false;
+        _open = !_open;
+    }
+}
