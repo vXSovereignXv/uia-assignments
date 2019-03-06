f91d208d3a49756fd8dc2d8e48b6b3974b334167
Nick Daigle <nickdaigle66@gmail.com>
2019-03-05 22:29:03 -0600

    Continue chapter 9

---
+++ ch09/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    private Dictionary<string, int> _items;
+
+    public void StartUp()
+    {
+        Debug.Log("Inventory manager starting...");
+
+        _items = new Dictionary<string, int>();
+
+        status = ManagerStatus.Started;
+    }
+
+    private void DisplayItems()
+    {
+        StringBuilder itemDisplay = new StringBuilder("Items: ");
+
+        foreach(var item in _items)
+        {
+            itemDisplay.Append($"{item.Key} ({item.Value}) ");
+        }
+
+        Debug.Log(itemDisplay.ToString());
+    }
+
+    public void AddItem(string name)
+    {
+        if(_items.ContainsKey(name))
+        {
+            _items[name] += 1;
+        }
+        else
+        {
+            _items[name] = 1;
+        }
+
+        DisplayItems();
+    }
+}
