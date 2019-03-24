966a8d90e7714fe1c88be7ffa9a4cf0fe0126243
Nick Daigle <nickdaigle66@gmail.com>
2019-03-24 15:36:18 -0500

    Start chapter 13

---
+++ ch12/Assets/Editor/TestPostBuild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class TestPostBuild
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        Debug.Log("build location: " + pathToBuiltProject);
+    }
+}
